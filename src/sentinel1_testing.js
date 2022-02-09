f4f0dc82dd4a14c0dfdf35fda3c9e743254a9b98
Matt Harrington <42118783+M-Harrington@users.noreply.github.com>
2022-02-09 14:43:31 -0500

    No major changes

--- src/sentinel1_testing.js
+++ src/sentinel1_testing.js
@@ -60,3 +60,43 @@ var images_create = function(date){
 
 date_list = date_list.map(images_create);
 print(date_list);
+
+// print(date_loc);
+// print(date_loc[0]);
+// var dates = date_loc.map(function(feature){
+  
+
+
+//   var dateRange = ee.DateRange(date.advance(-(dayRange/2), 'day'), date.advance(dayRange/2, 'day'));
+//   var image = rrs.filterDate(dateRange).mean();
+
+
+//   // Get image and sum for the time interval
+//   var precImage = ee.ImageCollection('UCSB-CHG/CHIRPS/DAILY')
+//                     .filterBounds(coord)
+//                     .filterDate(start, end) 
+//                     .select('precipitation')
+//                     .sum();
+
+  //   return feature.set( num);
+  // });
+  
+
+// print(dates.first());
+
+//get date, define range
+
+
+// var results = coord.map(function(feature){
+
+
+                     
+//   // Extract precipitation for the sampling sites
+//   var summed = precImage.reduceRegion({
+//       reducer: ee.Reducer.sum(),
+//       geometry: feature.geometry(),
+//       scale: 30
+//   });
+
+//   return feature.set(summed)
+// })
